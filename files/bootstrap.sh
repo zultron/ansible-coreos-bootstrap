f67e0cbade577fc5cd720a3b0d77af5042f87cd2
John Morris <john@zultron.com>
2018-06-11 20:34:06 -0500

    Upgrade pypy from 5.1.0 to 5.8.0
    https://github.com/defunctzombie/ansible-coreos-bootstrap/pull/46/files

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -8,16 +8,16 @@ if [[ -e $HOME/.bootstrapped ]]; then
   exit 0
 fi
 
-PYPY_VERSION=5.1.0
+PYPY_VERSION=5.8.0
 
-if [[ -e $HOME/pypy-$PYPY_VERSION-linux64.tar.bz2 ]]; then
-  tar -xjf $HOME/pypy-$PYPY_VERSION-linux64.tar.bz2
-  rm -rf $HOME/pypy-$PYPY_VERSION-linux64.tar.bz2
+if [[ -e $HOME/pypy2-v$PYPY_VERSION-linux64.tar.bz2 ]]; then
+  tar -xjf $HOME/pypy2-v$PYPY_VERSION-linux64.tar.bz2
+  rm -rf $HOME/pypy2-v$PYPY_VERSION-linux64.tar.bz2
 else
-  wget -O - https://bitbucket.org/pypy/pypy/downloads/pypy-$PYPY_VERSION-linux64.tar.bz2 |tar -xjf -
+  wget -O - https://bitbucket.org/pypy/pypy/downloads/pypy2-v$PYPY_VERSION-linux64.tar.bz2 |tar -xjf -
 fi
 
-mv -n pypy-$PYPY_VERSION-linux64 pypy
+mv -n pypy2-v$PYPY_VERSION-linux64 pypy
 
 ## library fixup
 mkdir -p pypy/lib
